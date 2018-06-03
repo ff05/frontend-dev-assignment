3f33f45315eaade22d1ddb7013fff1494f979684
frankfilius <frank.filius@accenture.com>
2018-06-03 09:52:32 +0200

    Adapt SearchBar to match exercise

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,13 +1,42 @@
 import React, { PureComponent } from 'react';
+import IconsSearch from '../Icons/Search';
+import IconsCancel from '../Icons/Cancel';
 import './SearchBar.css';
 
 class SearchBar extends PureComponent {
+  state = {
+    cancelVisible: false,
+    isFocus: false,
+    textInput: "",
+  }
+
+  handleFocus() {
+    this.setState({
+      isFocus: !this.state.isFocus,
+    });
+  }
+
+  handleClear() {
+    this.setState({
+      textInput: "",
+    });
+  }
+
   render() {
+    const { isFocus, cancelVisible } = this.state;
     return(
-      <div className="SearchBar">
+      <div className={isFocus ? "SearchBar SearchBar--highlight" : "SearchBar"}>
         <form className="form">
-          <input className="form__input" placeholder="Zoeken" />
-          {this.props.children}
+          <input
+            className="form__input"
+            type="text"
+            onFocus={this.handleFocus.bind(this)}
+            onBlur={this.handleFocus.bind(this)}
+            placeholder="Zoeken"
+            aria-label="Zoeken"
+          />
+          <IconsCancel isVisible={cancelVisible} onClick={this.handleClear.bind(this)} />
+          <IconsSearch isFocus={isFocus} />
         </form>
       </div>
     )
