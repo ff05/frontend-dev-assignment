fc2158710f2fd15e359bacb9f4665732d0c7a11c
frankfilius <frank.filius@accenture.com>
2018-06-03 14:48:03 +0200

    Expand SearchBar functionality to match exercise

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -9,19 +9,22 @@ class SearchBar extends PureComponent {
     isText: false,
     isFocus: false,
     textInput: "",
-  }
+    searchTerms: [],
+  };
 
   handleFocus() {
     this.setState({
       isFocus: !this.state.isFocus,
     });
-  }
+  };
 
-  handleClear() {
+  handleClear(event) {
+    event.preventDefault();
     this.setState({
       textInput: "",
+      searchTerms: [],
     });
-  }
+  };
 
   handleChange(event) {
     const { value } = event.target;
@@ -29,15 +32,41 @@ class SearchBar extends PureComponent {
       textInput: value,
       isText: value ? true : false,
      });
-  }
+  };
+
+  handleKeyUp(event) {
+    const { value } = event.target;
+    if (value.length >= 2) {
+      api(value)
+      .then(
+        (response) => this.getSearchTerms(response)
+      )
+      .catch(error => console.log(error))
+    }
+    if (this.state.searchTerms.length > 0)
+      this.setState({ searchTerms: [] })
+  };
   
   handleSearch(event) {
     event.preventDefault();
     api();
+  };
+
+  getSearchTerms(data) {
+    if (data) {
+      const terms = data.map((item) => ({
+        term: item.searchterm,
+        nr: item.nrResults,
+      }));
+      this.setState({
+        searchTerms: terms,
+      });
+    }
   }
 
   render() {
-    const { isFocus, isText, textInput } = this.state;
+    const { isFocus, isText, textInput, searchTerms } = this.state;
+    console.log(textInput);
     return(
       <div className={isFocus ? "SearchBar SearchBar--highlight" : "SearchBar"}>
         <form className="form">
@@ -47,6 +76,7 @@ class SearchBar extends PureComponent {
             onFocus={this.handleFocus.bind(this)}
             onBlur={this.handleFocus.bind(this)}
             onChange={this.handleChange.bind(this)}
+            onKeyUp={this.handleKeyUp.bind(this)}
             value={textInput}
             placeholder="Zoeken"
             aria-label="Zoeken"
@@ -54,6 +84,24 @@ class SearchBar extends PureComponent {
           <IconsCancel isText={isText} onClick={this.handleClear.bind(this)} />
           <IconsSearch onClick={this.handleSearch.bind(this)} isFocus={isFocus} />
         </form>
+        {searchTerms.length > 0 &&
+          <ul className="results-list">
+            {searchTerms.map((item, index) => {
+              const position = item.term.indexOf(textInput);
+              const sub = item.term.slice(position, position + textInput.length);
+              const left = item.term.slice(0, position);
+              const right = item.term.slice(position + textInput.length);
+              return (
+                <li className="results-list__result" key={item+index}>
+                  <span className="results-list__result__term1">{left}</span>
+                  <span className="results-list__result__term2">{sub}</span>
+                  <span className="results-list__result__term3">{right}</span>
+                  <span className="results-list__result__number"> ({item.nr})</span>
+                </li>
+              )
+            })}
+          </ul>
+        }
       </div>
     )
   }
